e9007811529e76456c8255c6a6d1501d7f966075
Daniel Berthereau <Daniel.git@Berthereau.net>
2022-07-18 00:00:00 +0000

    Released version 3.3.14.

--- data/scripts/upgrade.php
+++ data/scripts/upgrade.php
@@ -25,10 +25,17 @@ $settings = $services->get('Omeka\Settings');
 
 if (version_compare($oldVersion, '3.3.13.5', '<')) {
     $settings->set('bulkedit_deduplicate_on_save', true);
+}
 
+if (version_compare($oldVersion, '3.3.14', '<')) {
     $messenger = new Messenger();
     $message = new Message(
         'A new option was added to deduplicate values on save. It can be disabled in the main settings.' // @translate
     );
     $messenger->addWarning($message);
+
+    $message = new Message(
+        'It’s now possible to convert any data type to any data type.' // @translate
+    );
+    $messenger->addNotice($message);
 }

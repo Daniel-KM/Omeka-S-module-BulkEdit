20aeac54ffac7fcf47f16f25484f532f0932aa82
Daniel Berthereau <Daniel.git@Berthereau.net>
2023-01-09 00:00:00 +0000

    Used Omeka S v4 feature "element group" for settings.

--- src/Form/SettingsFieldset.php
+++ src/Form/SettingsFieldset.php
@@ -9,14 +9,20 @@ class SettingsFieldset extends Fieldset
 {
     protected $label = 'Bulk Edit'; // @translate
 
+    protected $elementGroups = [
+        'resources' => 'resources', // @translate
+    ];
+
     public function init(): void
     {
         $this
             ->setAttribute('id', 'bulk-edit')
+            ->setOption('element_groups', $this->elementGroups)
             ->add([
                 'name' => 'bulkedit_deduplicate_on_save',
                 'type' => Element\Checkbox::class,
                 'options' => [
+                    'element_group' => 'resources',
                     'label' => 'Deduplicate values on save', // @translate
                 ],
                 'attributes' => [

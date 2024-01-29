2736925047eac2af34e81dc04c6bab2494809515
Daniel Berthereau <Daniel.git@Berthereau.net>
2024-01-29 00:00:00 +0000

    Released version 3.4.23.

--- data/scripts/upgrade.php
+++ data/scripts/upgrade.php
@@ -53,3 +53,13 @@ if (version_compare($oldVersion, '3.3.16', '<')) {
     );
     $messenger->addSuccess($message);
 }
+
+if (version_compare($oldVersion, '3.4.23', '<')) {
+    if (!method_exists($this, 'checkModuleActiveVersion') || !$this->checkModuleActiveVersion('Common', '3.4.49')) {
+        $message = new Message(
+            'The module %1$s should be upgraded to version %2$s or later.', // @translate
+            'Common', '3.4.49'
+        );
+        throw new \Omeka\Module\Exception\ModuleCannotInstallException((string) $message);
+    }
+}

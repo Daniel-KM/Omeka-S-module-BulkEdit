e6c95bb64b6b16235e5d25c9f457bba699336933
Daniel Berthereau <Daniel.git@Berthereau.net>
2024-06-03 00:00:00 +0000

    Released version 3.4.30.

--- data/scripts/upgrade.php
+++ data/scripts/upgrade.php
@@ -2,31 +2,31 @@
 
 namespace BulkEdit;
 
-use Omeka\Stdlib\Message;
+use Common\Stdlib\PsrMessage;
 
 /**
  * @var Module $this
  * @var \Laminas\ServiceManager\ServiceLocatorInterface $services
- * @var string $oldVersion
  * @var string $newVersion
+ * @var string $oldVersion
  *
- * @var \Doctrine\ORM\EntityManager $entityManager
- * @var \Doctrine\DBAL\Connection $connection
  * @var \Omeka\Api\Manager $api
- * @var array $config
  * @var \Omeka\Settings\Settings $settings
+ * @var \Doctrine\DBAL\Connection $connection
+ * @var \Doctrine\ORM\EntityManager $entityManager
  * @var \Omeka\Mvc\Controller\Plugin\Messenger $messenger
  */
-// $entityManager = $services->get('Omeka\EntityManager');
-$connection = $services->get('Omeka\Connection');
-// $api = $services->get('Omeka\ApiManager');
-// $config = require dirname(__DIR__, 2) . '/config/module.config.php';
+$plugins = $services->get('ControllerPluginManager');
+$api = $plugins->get('api');
 $settings = $services->get('Omeka\Settings');
-$messenger = $services->get('ControllerPluginManager')->get('messenger');
+$translate = $plugins->get('translate');
+$connection = $services->get('Omeka\Connection');
+$messenger = $plugins->get('messenger');
+$entityManager = $services->get('Omeka\EntityManager');
 
 if (!method_exists($this, 'checkModuleActiveVersion') || !$this->checkModuleActiveVersion('Common', '3.4.58')) {
-    $message = new Message(
-        'The module %1$s should be upgraded to version %2$s or later.', // @translate
+    $message = new \Omeka\Stdlib\Message(
+        $translate('The module %1$s should be upgraded to version %2$s or later.'), // @translate
         'Common', '3.4.58'
     );
     throw new \Omeka\Module\Exception\ModuleCannotInstallException((string) $message);
@@ -37,27 +37,34 @@ if (version_compare($oldVersion, '3.3.13.5', '<')) {
 }
 
 if (version_compare($oldVersion, '3.3.14', '<')) {
-    $message = new Message(
+    $message = new PsrMessage(
         'A new option was added to deduplicate values on save. It can be disabled in the main settings.' // @translate
     );
     $messenger->addWarning($message);
 
-    $message = new Message(
+    $message = new PsrMessage(
         'It’s now possible to convert any data type to any data type.' // @translate
     );
     $messenger->addSuccess($message);
 }
 
 if (version_compare($oldVersion, '3.3.15', '<')) {
-    $message = new Message(
+    $message = new PsrMessage(
         'It’s now possible to update or remove the owner of resources.' // @translate
     );
     $messenger->addSuccess($message);
 }
 
 if (version_compare($oldVersion, '3.3.16', '<')) {
-    $message = new Message(
+    $message = new PsrMessage(
         'It’s now possible to get the Value Suggest uri from a label, when the remote endpoint returns a single result.' // @translate
+    );
+    $messenger->addSuccess($message);
+}
+
+if (version_compare($oldVersion, '3.4.30', '<')) {
+    $message = new PsrMessage(
+        'It’s now possible to add thumbnails and to remove media from items.' // @translate
     );
     $messenger->addSuccess($message);
 }
